e7d199104bab9ee2231a88dcf7ce4548fbe8b71d
hardyHSM <hardy.sebastian@yandex.ru>
2023-05-24 19:35:42 +0300

    fix replaceAll to replace

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -4,5 +4,10 @@ export function escapeRegExp(text) {
 
 export function decodeString(text) {
     if (!text) return null
-    return text.replaceAll('&amp;', '&').replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&quot;', '"').replaceAll('&#039;', '\'')
+    return text
+    .replace(/\&amp\;/g, '&')
+    .replace(/\&lt\;/g, '<')
+    .replace(/\&gt\;/g, '>')
+    .replace(/\&quot\;/g, '"')
+    .replace(/\&\#039\;/g, '\'')
 }
